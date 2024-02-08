d349c653f43aa9ff961afb636ddbf2c6d92c8b9d
minibase <minibase@minibaseui-Macmini.local>
2024-02-09 00:47:42 +0900

    수정

---
+++ src/routes/image.js
@@ -0,0 +1,19 @@
+import fs from 'fs';
+import aws from 'aws-sdk';
+import dotenv from 'dotenv';
+import express from 'express';
+import multers3 from 'multer-s3';
+import multer from 'multer';
+
+
+const router = express.Router();
+
+
+router.post('/image', async (req, res, next) => {
+
+});
+
+
+export default express;
+
+
