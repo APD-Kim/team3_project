14916fc6bb76670deaacc293717bcc20bface040
gimmingeon <mnbqwe123987@naver.com>
2024-02-09 19:21:02 +0900

    이메일 인증 추가

--- src/routes/user.router.js
+++ src/routes/user.router.js
@@ -8,12 +8,12 @@ import nodemailer from "nodemailer";
 const router = express.Router();
 
 // 랜덤한 4자리 숫자 생성
-const generateVerificationCode = () => {
+const randomnumber = () => {
   return Math.floor(1000 + Math.random() * 9000);
 };
 
 // 각 사용자에 대한 인증 코드 저장
-const verificationCodes = {};
+const verifiCode = {};
 
 const transporter = nodemailer.createTransport({
   service: "gmail",
@@ -23,9 +23,39 @@ const transporter = nodemailer.createTransport({
   },
 });
 
-// 회원가입 API
+// 이메일 코드 전송 API
+router.post("/verify-email", async (req, res, next) => {
+  const { email} = req.body;
+
+  if (!email) {
+    return res.status(400).json({ message: "이메일을 적지 않았습니다" });
+  }
+
+  const verificationCode = randomnumber();
+
+  // 사용자에 대한 인증 코드 저장
+  verifiCode[email] = verificationCode;
+
+  // 이메일 내용
+  const mailsend = {
+    from: process.env.SEND_MAIL_ID,
+    to: email,
+    subject: "이메일 인증",
+    text: `계정을 인증하려면 다음 숫자를 입력하세요: ${verificationCode}`,
+  };
+
+  transporter.sendMail(mailsend, (error) => {
+    if (error) {
+      return res.status(500).json({ message: '이메일 전송 중 오류가 발생했습니다.' });
+    } else {
+      return res.status(201).json({ message: '이메일을 확인해주세요.' });
+    }
+  });
+});
+
+// 회원가입 api
 router.post("/sign-up", async (req, res, next) => {
-  const { email, password, passwordconfirm, name } = req.body;
+  const { verificationCode, email, password, passwordconfirm, name } = req.body;
 
   if (!email) {
     return res.status(400).json({ message: "이메일을 적지 않았습니다" });
@@ -59,41 +89,17 @@ router.post("/sign-up", async (req, res, next) => {
 
   const hashedPassword = await bcrypt.hash(password, 10);
 
-  const user = await prisma.user.create({
-    data: { email, password: hashedPassword, name },
-  });
-
-  const verificationCode = generateVerificationCode();
-
-  // 사용자에 대한 인증 코드 저장
-  verificationCodes[email] = verificationCode;
-
-  const mailsend = {
-    from: process.env.SEND_MAIL_ID,
-    to: email,
-    subject: "이메일 인증",
-    text: `계정을 인증하려면 다음 숫자를 입력하세요: ${verificationCode}`,
-  };
-
-  transporter.sendMail(mailsend, (error) => {
-    if (error) {
-      return res.status(500).json({ message: '이메일 전송 중 오류가 발생했습니다.' });
-    } else {
-      return res.status(201).json({ data: user, message: '이메일을 확인해주세요.' });
-    }
-  });
-});
-
-// 이메일 인증 확인 API
-router.post("/verify-email", async (req, res, next) => {
-  const { email, verificationCode } = req.body;
-
   // 인증 코드 일치 여부 확인
-  if (verificationCodes[email] && verificationCodes[email] === parseInt(verificationCode)) {
-    // 코드 일치 시, 회원가입 프로세스 계속 진행
-    delete verificationCodes[email]; // 사용된 인증 코드 삭제
-    // ... (사용자 등록 계속 진행)
-    return res.status(201).json({ message: '이메일 인증이 성공적으로 완료되었습니다.' });
+  if (verifiCode[email] && verifiCode[email] === parseInt(verificationCode)) {
+
+    // 사용된 인증 코드 삭제
+    delete verifiCode[email]; 
+
+    const user = await prisma.user.create({
+           data: { email, password: hashedPassword, name },
+         });
+    
+    return res.status(201).json({ data: user, message: '이메일 인증이 성공적으로 완료되었습니다.' });
   } else {
     return res.status(400).json({ message: '이메일 인증이 실패했습니다. 올바른 숫자를 입력하세요.' });
   }
@@ -115,39 +121,11 @@ router.post("/login", async (req, res, next) => {
   return res.status(200).json({ message: "로그인에 성공하였습니다" });
 });
 
+
 // 로그아웃 api
 router.post("/logout", authMiddleware, async (req, res, next) => {
   res.clearCookie("authorization");
   return res.status(200).json({ message: "로그아웃에 성공하였습니다" });
 });
-
-// 회원 목록 조회 api
-router.get("/users", async (req, res, next) => {
-  try {
-    const users = await prisma.user.findMany();
-    return res.status(200).json({ data: users });
-  } catch (error) {
-    console.error(error);
-    return res.status(500).json({ message: '서버 오류가 발생했습니다.' });
-  }
-});
-
-// 회원 삭제 api
-router.delete("/user/:userId", async (req, res, next) => {
-  const { userId } = req.params;
-
-  try {
-    // 회원 삭제
-    const deletedUser = await prisma.user.delete({
-      where: { userId: parseInt(userId) },
-    });
-
-    return res.status(200).json({ data: deletedUser, message: '회원이 삭제되었습니다.' });
-  } catch (error) {
-    console.error(error);
-    return res.status(500).json({ message: '서버 오류가 발생했습니다.' });
-  }
-});
-
 
 export default router;
